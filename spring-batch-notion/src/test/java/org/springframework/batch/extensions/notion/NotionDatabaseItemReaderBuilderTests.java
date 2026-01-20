0ed0a86ad8f22cb00ee9314b27aad1e5cd4d2e78
qlfyd123 <hajewoong@gmail.com>
2026-01-20 14:44:01 +0900

    Accept the reviewed changes and fix the incorrect test code.
    Signed-off-by: qlfyd123 <hajewoong@gmail.com>

--- spring-batch-notion/src/test/java/org/springframework/batch/extensions/notion/NotionDatabaseItemReaderBuilderTests.java
+++ spring-batch-notion/src/test/java/org/springframework/batch/extensions/notion/NotionDatabaseItemReaderBuilderTests.java
@@ -16,55 +16,55 @@
 
 package org.springframework.batch.extensions.notion;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.batch.extensions.notion.mapping.PropertyMapper;
-import org.springframework.test.util.ReflectionTestUtils;
+
+import static org.assertj.core.api.BDDAssertions.catchThrowable;
+import static org.assertj.core.api.BDDAssertions.then;
 
 class NotionDatabaseItemReaderBuilderTests {
 
 	@Test
 	void should_succeed() {
 		// GIVEN
-		String expectedToken = "FOO TOKEN";
-		String expectedDatabaseId = "FOO DATABASE ID";
-		PropertyMapper<String> expectedPropertyMapper = properties -> "FOO PROPERTY";
-		Filter expectedFilter = Filter.where().checkbox("IsActive").isEqualTo(true);
-		String expectedName = "FOO NAME";
-		Sort[] expectedSorts = new Sort[0];
-		int expectedPageSize = 50;
-		boolean expectedSaveState = true;
-		int expectedMaxItemCount = 1000;
-		int expectedCurrentItemCount = 10;
-		String expectedBaseUrl = "https://example.com";
+		String token = "FOO TOKEN";
+		String databaseId = "FOO DATABASE ID";
+		PropertyMapper<String> propertyMapper = properties -> "FOO PROPERTY";
+		Filter filter = Filter.where().checkbox("IsActive").isEqualTo(true);
+		String name = "FOO NAME";
+		Sort[] sorts = new Sort[0];
+		int pageSize = 50;
+		boolean saveState = true;
+		int maxItemCount = 1000;
+		int currentItemCount = 10;
+		String baseUrl = "https://example.com";
 
 		// WHEN
-		NotionDatabaseItemReader<String> reader = new NotionDatabaseItemReaderBuilder<String>().token(expectedToken)
-			.databaseId(expectedDatabaseId)
-			.propertyMapper(expectedPropertyMapper)
-			.filter(expectedFilter)
-			.name(expectedName)
-			.sorts(expectedSorts)
-			.pageSize(expectedPageSize)
-			.saveState(expectedSaveState)
-			.maxItemCount(expectedMaxItemCount)
-			.currentItemCount(expectedCurrentItemCount)
-			.baseUrl(expectedBaseUrl)
+		NotionDatabaseItemReader<String> reader = new NotionDatabaseItemReaderBuilder<String>().token(token)
+			.databaseId(databaseId)
+			.propertyMapper(propertyMapper)
+			.filter(filter)
+			.name(name)
+			.sorts(sorts)
+			.pageSize(pageSize)
+			.saveState(saveState)
+			.maxItemCount(maxItemCount)
+			.currentItemCount(currentItemCount)
+			.baseUrl(baseUrl)
 			.build();
 
 		// THEN
-		Assertions.assertThat(ReflectionTestUtils.getField(reader, "token")).isEqualTo(expectedToken);
-		Assertions.assertThat(ReflectionTestUtils.getField(reader, "databaseId")).isEqualTo(expectedDatabaseId);
-		Assertions.assertThat(ReflectionTestUtils.getField(reader, "propertyMapper")).isEqualTo(expectedPropertyMapper);
-		Assertions.assertThat(ReflectionTestUtils.getField(reader, "filter")).isEqualTo(expectedFilter);
-		Assertions.assertThat(ReflectionTestUtils.getField(reader, "sorts")).isEqualTo(expectedSorts);
-		Assertions.assertThat(ReflectionTestUtils.getField(reader, "pageSize")).isEqualTo(expectedPageSize);
-		Assertions.assertThat(ReflectionTestUtils.getField(reader, "saveState")).isEqualTo(expectedSaveState);
-		Assertions.assertThat(ReflectionTestUtils.getField(reader, "name")).isEqualTo(expectedName);
-		Assertions.assertThat(ReflectionTestUtils.getField(reader, "maxItemCount")).isEqualTo(expectedMaxItemCount);
-		Assertions.assertThat(ReflectionTestUtils.getField(reader, "currentItemCount"))
-			.isEqualTo(expectedCurrentItemCount);
-		Assertions.assertThat(ReflectionTestUtils.getField(reader, "baseUrl")).isEqualTo(expectedBaseUrl);
+		then(reader).extracting("token").isEqualTo(token);
+		then(reader).extracting("databaseId").isEqualTo(databaseId);
+		then(reader).extracting("propertyMapper").isEqualTo(propertyMapper);
+		then(reader).extracting("filter").isEqualTo(filter);
+		then(reader).extracting("sorts").isEqualTo(sorts);
+		then(reader).extracting("pageSize").isEqualTo(pageSize);
+		then(reader).extracting("saveState").isEqualTo(saveState);
+		then(reader).extracting("name").isEqualTo(name);
+		then(reader).extracting("maxItemCount").isEqualTo(maxItemCount);
+		then(reader).extracting("currentItemCount").isEqualTo(currentItemCount);
+		then(reader).extracting("baseUrl").isEqualTo(baseUrl);
 	}
 
 	@Test
@@ -86,8 +86,8 @@ class NotionDatabaseItemReaderBuilderTests {
 		NotionDatabaseItemReader<String> reader = builder.build();
 
 		// THEN
-		Assertions.assertThat(ReflectionTestUtils.getField(reader, "saveState")).isEqualTo(false);
-		Assertions.assertThat(ReflectionTestUtils.getField(reader, "name")).isNull();
+		then(reader).extracting("saveState").isEqualTo(false);
+		then(reader).extracting("name").isEqualTo(NotionDatabaseItemReader.class.getSimpleName());
 	}
 
 	@Test
@@ -95,10 +95,15 @@ class NotionDatabaseItemReaderBuilderTests {
 		// GIVEN
 		NotionDatabaseItemReaderBuilder<Object> builder = new NotionDatabaseItemReaderBuilder<>().token(null)
 			.databaseId("FOO DATABASE ID")
-			.propertyMapper(properties -> "FOO PROPERTY");
+			.propertyMapper(properties -> "FOO PROPERTY")
+			.saveState(false);
 
-		// WHEN & THEN
-		Assertions.assertThatThrownBy(builder::build).isInstanceOf(NullPointerException.class);
+		// WHEN
+		Throwable exception = catchThrowable(builder::build);
+
+		// THEN
+		then(exception).isInstanceOf(IllegalArgumentException.class);
+		then(exception).hasMessage("token, databaseId, and propertyMapper must not be null");
 	}
 
 	@Test
@@ -106,10 +111,15 @@ class NotionDatabaseItemReaderBuilderTests {
 		// GIVEN
 		NotionDatabaseItemReaderBuilder<Object> builder = new NotionDatabaseItemReaderBuilder<>().token("FOO TOKEN")
 			.databaseId(null)
-			.propertyMapper(properties -> "FOO PROPERTY");
+			.propertyMapper(properties -> "FOO PROPERTY")
+			.saveState(false);
 
-		// WHEN & THEN
-		Assertions.assertThatThrownBy(builder::build).isInstanceOf(NullPointerException.class);
+		// WHEN
+		Throwable exception = catchThrowable(builder::build);
+
+		// THEN
+		then(exception).isInstanceOf(IllegalArgumentException.class);
+		then(exception).hasMessage("token, databaseId, and propertyMapper must not be null");
 	}
 
 	@Test
@@ -117,10 +127,15 @@ class NotionDatabaseItemReaderBuilderTests {
 		// GIVEN
 		NotionDatabaseItemReaderBuilder<Object> builder = new NotionDatabaseItemReaderBuilder<>().token("FOO TOKEN")
 			.databaseId("FOO DATABASE ID")
-			.propertyMapper(null);
+			.propertyMapper(null)
+			.saveState(false);
 
-		// WHEN & THEN
-		Assertions.assertThatThrownBy(builder::build).isInstanceOf(NullPointerException.class);
+		// WHEN
+		Throwable exception = catchThrowable(builder::build);
+
+		// THEN
+		then(exception).isInstanceOf(IllegalArgumentException.class);
+		then(exception).hasMessage("token, databaseId, and propertyMapper must not be null");
 	}
 
 	@Test
@@ -138,9 +153,11 @@ class NotionDatabaseItemReaderBuilderTests {
 			.saveState(saveState)
 			.name(name);
 
-		// WHEN & THEN
-		Assertions.assertThatThrownBy(builder::build)
-			.isInstanceOf(IllegalArgumentException.class)
+		// WHEN
+		Throwable exception = catchThrowable(builder::build);
+
+		// THEN
+		then(exception).isInstanceOf(IllegalArgumentException.class)
 			.hasMessage("A name is required when saveState is set to true.");
 	}
 
@@ -159,9 +176,11 @@ class NotionDatabaseItemReaderBuilderTests {
 			.saveState(saveState)
 			.name(name);
 
-		// WHEN & THEN
-		Assertions.assertThatThrownBy(builder::build)
-			.isInstanceOf(IllegalArgumentException.class)
+		// WHEN
+		Throwable exception = catchThrowable(builder::build);
+
+		// THEN
+		then(exception).isInstanceOf(IllegalArgumentException.class)
 			.hasMessage("A name is required when saveState is set to true.");
 	}
 
@@ -180,9 +199,11 @@ class NotionDatabaseItemReaderBuilderTests {
 			.name(name)
 			.pageSize(pageSize);
 
-		// WHEN & THEN
-		Assertions.assertThatThrownBy(builder::build)
-			.isInstanceOf(IllegalArgumentException.class)
+		// WHEN
+		Throwable exception = catchThrowable(builder::build);
+
+		// THEN
+		then(exception).isInstanceOf(IllegalArgumentException.class)
 			.hasMessage("pageSize must be less than or equal to 100");
 	}
 
@@ -201,10 +222,11 @@ class NotionDatabaseItemReaderBuilderTests {
 			.name(name)
 			.pageSize(pageSize);
 
-		// WHEN & THEN
-		Assertions.assertThatThrownBy(builder::build)
-			.isInstanceOf(IllegalArgumentException.class)
-			.hasMessage("pageSize must be greater than zero");
+		// WHEN
+		Throwable exception = catchThrowable(builder::build);
+
+		// THEN
+		then(exception).isInstanceOf(IllegalArgumentException.class).hasMessage("pageSize must be greater than zero");
 	}
 
 }
