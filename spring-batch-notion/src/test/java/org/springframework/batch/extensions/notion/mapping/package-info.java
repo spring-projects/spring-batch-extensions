4de2a4a14630216fd2231c2c303308eaa17c9f9d
Stefano Cordio <stefano.cordio@gmail.com>
2026-01-31 11:54:12 +0100

    Bump `nullaway` from 0.12.11 to 0.13.1
    This also sets `RequireExplicitNullMarking` to WARN.
    
    Signed-off-by: Stefano Cordio <stefano.cordio@gmail.com>

--- spring-batch-notion/src/test/java/org/springframework/batch/extensions/notion/mapping/package-info.java
+++ spring-batch-notion/src/test/java/org/springframework/batch/extensions/notion/mapping/package-info.java
@@ -13,10 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/**
- * Tests for Spring Batch Notion.
- */
 @NullUnmarked
-package org.springframework.batch.extensions.notion;
+package org.springframework.batch.extensions.notion.mapping;
 
 import org.jspecify.annotations.NullUnmarked;
